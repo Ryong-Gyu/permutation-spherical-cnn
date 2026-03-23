12162348ea5a6a4db61d1a6fbfdeeb1a3009ed80
Ryong-Gyu Lee <dlfydrb@gmail.com>
2026-03-23 11:10:39 +0900

    Refocus package on cubic sampling

--- __init__.py
+++ __init__.py
@@ -1,41 +1,15 @@
 from .stencil_generator import LagrangeFilter3D
-from .spherical_canonicalizer import (
-    compute_sample_scores,
-    choose_perm_from_tables,
-    apply_perm_to_tap_values,
-    build_equator_and_perm_tables,
-    SphericalTapCanonicalizer,
-)
-from .spherical_topology import (
-    normalize_points,
-    build_rotation_matrices_about_z,
-    build_minimal_rotation_matrix,
-    build_nfold_symmetric_spherical_grid,
-    invert_permutation,
-    compose_permutations,
-    build_tilt_permutations_to_north,
-    build_all_permutations_from_rotations,
-    build_tilt_spin_permutation_bank,
-    build_rotation_matrices_from_axis_permutations,
+from .cubic_sampling import (
+    build_axis_rotation_matrix,
+    build_cubic_sampling_points,
+    build_cubic_sampling_stencils,
 )
 from .stencil_conv3d import StencilConv3d
 
 __all__ = [
     "LagrangeFilter3D",
-    "compute_sample_scores",
-    "choose_perm_from_tables",
-    "apply_perm_to_tap_values",
-    "build_equator_and_perm_tables",
-    "SphericalTapCanonicalizer",
-    "normalize_points",
-    "build_rotation_matrices_about_z",
-    "build_minimal_rotation_matrix",
-    "build_nfold_symmetric_spherical_grid",
-    "invert_permutation",
-    "compose_permutations",
-    "build_tilt_permutations_to_north",
-    "build_all_permutations_from_rotations",
-    "build_tilt_spin_permutation_bank",
-    "build_rotation_matrices_from_axis_permutations",
+    "build_axis_rotation_matrix",
+    "build_cubic_sampling_points",
+    "build_cubic_sampling_stencils",
     "StencilConv3d",
 ]
